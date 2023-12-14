af27e4dff6877299536e9fe937a78599bd56725a
Josh Zillwood <joshzillwood@gmail.com>
2023-12-13 22:28:56 -0600

    Added HmacPolicyBuilder documentation.

--- Source/Policies/HmacPolicyBuilder.cs
+++ Source/Policies/HmacPolicyBuilder.cs
@@ -4,6 +4,9 @@ using HmacManagement.Headers;
 
 namespace HmacManagement.Policies;
 
+/// <summary>
+/// A class representing a builder for a HmacPolicy.
+/// </summary>
 public class HmacPolicyBuilder
 {
     protected readonly KeyCredentials Keys = new();
@@ -11,30 +14,55 @@ public class HmacPolicyBuilder
     protected readonly Nonce Nonce = new();
     protected readonly HeaderSchemeCollection HeaderSchemes = new();
 
+    /// <summary>
+    /// Uses the specified guid as the public key for this policy.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>An instance of HmacPolicyBuilder.</returns>
     public HmacPolicyBuilder UsePublicKey(Guid key)
     {
         Keys.PublicKey = key;
         return this;
     }
 
+    /// <summary>
+    /// Uses the specified base64 string as the private key for this policy.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>An instance of HmacPolicyBuilder.</returns>
     public HmacPolicyBuilder UsePrivateKey(string key)
     {
         Keys.PrivateKey = key;
         return this;
     }
 
+    /// <summary>
+    /// Uses the specified algorithm to hash request content as part of the signature.
+    /// </summary>
+    /// <param name="hashAlgorithm"></param>
+    /// <returns>An instance of HmacPolicyBuilder.</returns>
     public HmacPolicyBuilder UseContentHashAlgorithm(ContentHashAlgorithm hashAlgorithm)
     {
         Algorithms.ContentHashAlgorithm = hashAlgorithm;
         return this;
     }
 
+    /// <summary>
+    /// Uses the specified algorithm to sign the mac.
+    /// </summary>
+    /// <param name="hashAlgorithm"></param>
+    /// <returns>An instance of HmacPolicyBuilder.</returns>
     public HmacPolicyBuilder UseSigningHashAlgorithm(SigningHashAlgorithm hashAlgorithm)
     {
         Algorithms.SigningHashAlgorithm = hashAlgorithm;
         return this;
     }
 
+    /// <summary>
+    /// Sets the maximum age on an http request and the ttl on the memory cache entry for the request nonce.
+    /// </summary>
+    /// <param name="maxAge"></param>
+    /// <returns>An instance of HmacPolicyBuilder.</returns>
     public HmacPolicyBuilder UseInMemoryCache(TimeSpan maxAge)
     {
         Nonce.CacheName = "InMemory";
@@ -42,6 +70,11 @@ public class HmacPolicyBuilder
         return this;
     }
 
+    /// <summary>
+    /// Sets the maximum age on an http request and the ttl on the distributed cache entry for the request nonce.
+    /// </summary>
+    /// <param name="maxAge"></param>
+    /// <returns>An instance of HmacPolicyBuilder.</returns>
     public HmacPolicyBuilder UseDistributedCache(TimeSpan maxAge)
     {
         Nonce.CacheName = "Distributed";
@@ -49,12 +82,22 @@ public class HmacPolicyBuilder
         return this;
     }
 
+    /// <summary>
+    /// Adds a specified scheme to the HeaderSchemes collection.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="configureScheme"></param>
+    /// <returns>An instance of HmacPolicyBuilder.</returns>
     public HmacPolicyBuilder AddScheme(string name, Action<HeaderScheme> configureScheme)
     {
         HeaderSchemes.Add(name, configureScheme);   
         return this;
     }
 
+    /// <summary>
+    /// Builds an instance of HmacPolicy.
+    /// </summary>
+    /// <returns>An instance of the configured HmacPolicy.</returns>
     public HmacPolicy Build()
     {
         if (string.IsNullOrWhiteSpace(Nonce.CacheName))
