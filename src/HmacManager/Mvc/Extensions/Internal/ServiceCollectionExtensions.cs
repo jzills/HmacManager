bebb3c8a7c1e1b59125a6e174938aeb749294ee4
Joshua Zillwood <42525520+jzills@users.noreply.github.com>
2024-06-03 20:09:21 -0500

    Updated NonceCacheCollection DI registration to avoid using BuildServiceProvider.

--- src/HmacManager/Mvc/Extensions/Internal/ServiceCollectionExtensions.cs
+++ src/HmacManager/Mvc/Extensions/Internal/ServiceCollectionExtensions.cs
@@ -3,11 +3,10 @@ using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using HmacManager.Caching;
-using HmacManager.Caching.Distributed;
-using HmacManager.Caching.Memory;
 using HmacManager.Common;
 using HmacManager.Components;
 using HmacManager.Policies;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace HmacManager.Mvc.Extensions.Internal;
 
@@ -31,59 +30,18 @@ internal static class ServiceCollectionExtensions
 
     internal static IServiceCollection AddCacheCollection(this IServiceCollection services)
     {
-        var caches = new NonceCacheCollection();
+        services.TryAddSingleton<IMemoryCache, MemoryCache>();
+        services.TryAddSingleton<IDistributedCache, MemoryDistributedCache>();
 
         return services
-            .AddMemoryCacheIfNotExists(caches)
-            .AddDistributedCacheIfNotExists(caches)
-            .AddScoped<IComponentCollection<INonceCache>, NonceCacheCollection>(_ => caches);
+            .AddScoped<IComponentCollection<INonceCache>, NonceCacheCollection>(GetCacheCollection);
     }
 
     internal static IServiceCollection AddFactory(
         this IServiceCollection services
     ) => services.AddScoped<IHmacManagerFactory, HmacManagerFactory>();
 
-    internal static IServiceCollection AddMemoryCacheIfNotExists(this IServiceCollection services, NonceCacheCollection caches)
-    {
-        var serviceProvider = services.BuildServiceProvider();
-
-        // Add memory cache if it isn't registered
-        var memoryCache = serviceProvider.GetService<IMemoryCache>();
-        if (memoryCache is null)
-        {
-            services.AddMemoryCache();
-            serviceProvider = services.BuildServiceProvider();
-            memoryCache = serviceProvider.GetRequiredService<IMemoryCache>();
-        }
-
-        caches.Add(NonceCacheType.Memory, new NonceMemoryCache(memoryCache, new NonceCacheOptions
-        { 
-            CacheType = NonceCacheType.Memory,
-            MaxAgeInSeconds = 30
-        }));
-
-        return services;
-    }
-
-    internal static IServiceCollection AddDistributedCacheIfNotExists(this IServiceCollection services, NonceCacheCollection caches)
-    {
-        var serviceProvider = services.BuildServiceProvider();
-
-        // Add distributed cache if it isn't registered
-        var distributedCache = serviceProvider.GetService<IDistributedCache>();
-        if (distributedCache is null)
-        {
-            services.AddDistributedMemoryCache();
-            serviceProvider = services.BuildServiceProvider();
-            distributedCache = serviceProvider.GetRequiredService<IDistributedCache>();
-        }
-
-        caches.Add(NonceCacheType.Distributed, new NonceDistributedCache(distributedCache, new NonceCacheOptions
-        {
-            CacheType = NonceCacheType.Distributed, 
-            MaxAgeInSeconds = 30 
-        }));
-
-        return services;
-    }
+    private static NonceCacheCollection GetCacheCollection(
+        IServiceProvider serviceProvider
+    ) => new NonceCacheCollection().AddDefaultCaches(serviceProvider);
 }
