d8f025211c929a7a27a9c5935ec14d69ea18d80c
Joshua Zillwood <42525520+jzills@users.noreply.github.com>
2024-06-08 17:40:58 -0500

    Added brief documentation to hmac_policy_generator script file.

--- test/Data/hmac_policy_generator.py
+++ test/Data/hmac_policy_generator.py
@@ -7,35 +7,66 @@ import string
 NUM_POLICIES = 1000
 
 def hmac_policy_generator():
+    """
+    Generates a single hmac policy object.
+    """
+
     def generate_uuid():
+        """
+        Generates an UUID public key.
+        """
+
         return str(uuid.uuid4())
 
     def generate_base64_string():
+        """
+        Generates a base64 encoded private key.
+        """
+        
         return base64.b64encode(uuid.uuid4().bytes).decode('utf-8')
 
     def generate_content_hash_algorithm():
+        """
+        Generates a random content hash algorithm.
+        """
+        
         return random.choice(["SHA1", "SHA256", "SHA512"])
 
     def generate_signing_algorithm():
+        """
+        Generates a random signing hash algorithm.
+        """
+
         return random.choice(["HMACSHA1", "HMACSHA256", "HMACSHA512"])
 
     def generate_cache_type():
+        """
+        Generates a random cache type.
+        """
+
         return random.choice(["Memory", "Distributed"])
 
     def generate_non_empty_string():
+        """
+        Generates a random non empty string.
+        """
+
         allowed_chars = string.ascii_letters + string.digits + '-_'
         length = random.randint(5, 15)  # Generate strings of length between 5 and 15
         return ''.join(random.choices(allowed_chars, k=length))
 
     def generate_headers():
+        """
+        Generates a randomized variable length set of headers.
+        """
+
         headers = []
-        for i in range(random.randint(1, 5)):
-            header_name = generate_non_empty_string()
-            claim_type = generate_non_empty_string()
+        for _ in range(random.randint(1, 5)):
             headers.append({
-                "Name": header_name,
-                "ClaimType": claim_type
+                "Name": generate_non_empty_string(),
+                "ClaimType": generate_non_empty_string()
             })
+            
         return headers
 
     random_data = {
